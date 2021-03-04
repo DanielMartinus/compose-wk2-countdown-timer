2dd75b7499a25f90d2461669975c825abd595267
Dion Segijn <dionsegijn@hotmail.com>
2021-03-04 23:36:16 +0100

    Fix formatting

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -22,17 +22,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,11 +41,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.androiddevchallenge.ui.theme.MyTheme
-import androidx.compose.ui.Alignment
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.concurrent.TimeUnit
-
 
 class CountdownViewModel : ViewModel() {
 
@@ -116,12 +115,16 @@ fun CountdownScreen(viewModel: CountdownViewModel = CountdownViewModel()) {
         is CountdownViewModel.State.Started -> "Pause"
         is CountdownViewModel.State.Idle -> "Start"
     }
-    CountdownContent(countdown = countdownTime, ctaButtonText = ctaButtonText, ctaClick = {
-        when (state) {
-            is CountdownViewModel.State.Started -> viewModel.stop()
-            is CountdownViewModel.State.Idle -> viewModel.start()
-        }
-    }, reset = { viewModel.reset() })
+    CountdownContent(
+        countdown = countdownTime, ctaButtonText = ctaButtonText,
+        ctaClick = {
+            when (state) {
+                is CountdownViewModel.State.Started -> viewModel.stop()
+                is CountdownViewModel.State.Idle -> viewModel.start()
+            }
+        },
+        reset = { viewModel.reset() }
+    )
 }
 
 @Composable
--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Theme.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Theme.kt
@@ -19,7 +19,6 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = darkBlue,
